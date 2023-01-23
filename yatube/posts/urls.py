35c32368ef7ebb4ae91b5353e4549cafa42551f6
JRushFobos <fobos_media@mail.ru>
2023-01-23 13:46:30 +0500

    project ready for review #1

---
+++ yatube/posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    #Главная страница
+    path('', views.index),
+    path('index/', views.index, name='index'),
+    #Список групп
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+
+    ]
