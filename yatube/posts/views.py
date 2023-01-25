7ebbf5281651f482b5ea42bad5f169f40d8245f7
JRushFobos <fobos_media@mail.ru>
2023-01-26 02:32:27 +0500

    project ready for review #4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -11,7 +11,6 @@ def index(request):
     }
     return render(request, template, context)
 
-
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = (Post.objects.filter(group=group).order_by('-pub_date')
