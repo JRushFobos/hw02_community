dc64bf4ae5775175e3a123688f443932de497069
JRushFobos <fobos_media@mail.ru>
2023-01-26 02:29:58 +0500

    project ready for review #4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from .models import Post, Group
 
 
-
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:settings.NUM_POSTS]
     template = 'posts/index.html'
@@ -15,7 +14,8 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:settings.NUM_POSTS]
+    posts = (Post.objects.filter(group=group).order_by('-pub_date')
+            [:settings.NUM_POSTS])
     template = 'posts/group_list.html'
     context = {
         'group': group,
